c9f2fb87fe25f9e0a36cb96b077c5edcac8ca27b
Kristin Aoki <you@example.com>
2020-02-06 20:09:34 -0500

    Add new print statements to check memory addresses

--- exercises/ex02/aspace.c
+++ exercises/ex02/aspace.c
@@ -10,17 +10,31 @@ License: GNU GPLv3
 
 int var1;
 
+void address() {
+
+  int var3 = 6;
+  printf ("Address of var3 is %p\n", &var3);
+}
+
 int main ()
 {
     int var2 = 5;
     void *p = malloc(128);
     char *s = "Hello, World";
+    void *m = malloc(128);
+    void *chunk1 = malloc(32);
+    void *chunk2 = malloc(32);
+
+    address();
 
     printf ("Address of main is %p\n", main);
     printf ("Address of var1 is %p\n", &var1);
     printf ("Address of var2 is %p\n", &var2);
     printf ("p points to %p\n", p);
     printf ("s points to %p\n", s);
+    printf ("m points to %p\n", m);
+    printf ("chunk1 points to %p\n", chunk1);
+    printf ("chunk2 points to %p\n", chunk2);
 
     return 0;
 }
