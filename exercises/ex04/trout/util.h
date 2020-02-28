a8c3574983197068d6a93e6aafa0d2dc7304d264
Kristin Aoki <kaoki@olin.edu>
2020-02-27 19:08:17 -0500

    Delete extra header files

--- exercises/ex04/trout/util.h
+++ exercises/ex04/trout/util.h
@@ -1,24 +1,3 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <time.h>
-#include <netinet/in.h>
-#include <netinet/in_systm.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <netinet/udp.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <getopt.h>
-#include <sys/un.h>
-#include <stdarg.h>
-#include <syslog.h>
-#include <unistd.h>
 #include "trout.h"
 
 void err_doit (int errnoflag, int level, char *fmt, va_list ap);
